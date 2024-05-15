e826711d4a6758f297db6947d0b6eda36f291fba
thaisavieira <thaisavieira.tech@proton.me>
2024-05-15 00:42:02 -0300

    :lipstick: feat: Estilização CSS do container e :sparkles: inicializando variávies e criando funções

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ let gold = 50;
 let currentWeapon = 0;
 let fighting;
 let monsterHealth;
-let inventory = ["stick","dagger","sword"];
+let inventory = ["stick", "dagger", "sword"];
 
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
@@ -18,10 +18,37 @@ const monsterNameText = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
 //initialize buttons with the functions that will be called
-button1.onclick = goStore; 
+button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
-function goStore(){
-    console.log("Going to store")
+function goTown() {
+  button1.innerText = "Go to store";
+  button2.innerText = "Go to cave";
+  button3.innerText = "Fight dragon";
+  button1.onclick = goStore;
+  button2.onclick = goCave;
+  button3.onclick = fightDragon;
+  text.innerText = "You are in the town square. You see a sign that says \"store\"";
 }
+
+function goStore() {
+  button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)";
+  button3.innerText = "Go to town square";
+  button1.onclick = buyHealth;
+  button2.onclick = buyWeapon;
+  button3.onclick = goTown;
+  text.innerText = "You enter the store.";
+}
+
+function goCave() {
+  console.log("Going to cave");
+}
+
+function fightDragon() {
+  console.log("Fighting dragon");
+}
+
+function buyHealth() {}
+function buyWeapon() {}
